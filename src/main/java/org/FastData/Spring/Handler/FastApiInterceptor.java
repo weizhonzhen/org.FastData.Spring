24c1f6f3f7e7ec4930f78d5ec52c3c2f02f6e9bc
weizhonzhen <czdswzz@126.com>
2021-05-25 20:15:41 +0800

    Add files via upload

---
+++ src/main/java/org/FastData/Spring/Handler/FastApiInterceptor.java
@@ -0,0 +1,169 @@
+package org.FastData.Spring.Handler;
+
+import com.alibaba.fastjson.JSON;
+import org.FastData.Spring.Base.MapXml;
+import org.FastData.Spring.Context.DataContext;
+import org.FastData.Spring.Model.*;
+import org.FastData.Spring.Repository.IFastRepository;
+import org.FastData.Spring.Util.CacheUtil;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.*;
+
+@Component
+public class FastApiInterceptor implements HandlerInterceptor {
+    public final static String page = "page";
+    public final static String pageAll = "pageall";
+    public final static String param = "param";
+    public final static String all = "all";
+    public final static String write = "write";
+
+    @Resource
+    IFastRepository ifast;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Map param = request.getParameterMap();
+        String name = request.getAttribute("org.springframework.web.servlet.HandlerMapping.pathWithinHandlerMapping").toString().toLowerCase();
+        Map<String, Object> map = new HashMap<String, Object>();
+        Map<String, Object> data = new HashMap<String, Object>();
+
+        if (name != null && CacheUtil.exists(name.toLowerCase()) && mapDb(name) != null) {
+            for (String item : mapParam(name)) {
+                if (param.keySet().stream().anyMatch(a -> a.toString().equalsIgnoreCase(item))) {
+                    Object key = param.keySet().stream().filter(k -> k.toString().equalsIgnoreCase(item)).findFirst().get();
+                    String value = ((String[]) param.get(key))[0];
+
+                    if (value == null)
+                        continue;
+
+                    if (mapRequired(name, item)) {
+                        if (!value.equals(""))
+                            map.put(item, value);
+                        else {
+                            data.put("error", String.format("%s:不能为空", item));
+                            data.put("success", false);
+                            return getResult(response, data);
+                        }
+                    }
+
+                    int len = mapMaxlength(name, item);
+                    if (len != 0 && value.length() > len) {
+                        data.put("error", String.format("%s:最大长度%s", item, len));
+                        data.put("success", false);
+                        return getResult(response, data);
+                    } else
+                        map.put(item, value);
+                }
+            }
+
+            try (DataContext db = new DataContext(mapDb(name))) {
+                MapResult result = MapXml.getMapSql(name, map);
+                if (mapType(name).equalsIgnoreCase(page) || mapType(name).equalsIgnoreCase(pageAll)) {
+                    PageModel page = new PageModel();
+                    Optional pageSizeKey = param.keySet().stream().filter(k -> k.toString().equalsIgnoreCase("pagesize")).findFirst();
+                    if (pageSizeKey == Optional.empty())
+                        page.pageSize = 10;
+                    else {
+                        Object pageSize = ((String[])param.get(pageSizeKey.get()))[0];
+                        page.pageSize = pageSize == null ? 10 : Integer.parseInt(pageSize.toString());
+                    }
+
+                    Optional pageIdKey = param.keySet().stream().filter(k -> k.toString().equalsIgnoreCase("pageid")).findFirst();
+                    if (pageIdKey == Optional.empty())
+                        page.pageId = 1;
+                    else {
+                        Object pageId = ((String[])param.get((pageIdKey).get()))[0];
+                        page.pageId = pageId == null ? 1 : Integer.parseInt(pageId.toString());
+                    }
+
+                    PageResult pageInfo = db.page(page, result);
+
+                    data.put("success", true);
+                    map.put("data", pageInfo.list);
+                    map.put("page", pageInfo.pModel);
+                    return getResult(response, map);
+                }
+
+                if (mapType(name).equalsIgnoreCase(all)) {
+                    data.put("success", true);
+                    map.put("data", db.query(result).list);
+                    return getResult(response, map);
+                }
+
+                if (mapType(name).equalsIgnoreCase(write) && map.size() > 0) {
+                    DataReturn info = db.execute(result);
+                    data.put("success", info.writeReturn.isSuccess);
+                    if (!info.writeReturn.isSuccess)
+                        data.put("error", info.writeReturn.message);
+                    return getResult(response, map);
+                }
+
+                if (map.size() > 0) {
+                    data.put("success", true);
+                    map.put("data", db.query(result).list);
+                    return getResult(response, map);
+                }
+            }
+        }
+        return true;
+    }
+
+    private int mapMaxlength(String name, String param) {
+        try {
+            String len = CacheUtil.get(String.format("%s.%s.maxlength", name.toLowerCase(), param.toLowerCase()));
+            if (len == null || len.equals(""))
+                return 0;
+            else
+                return Integer.parseInt(len);
+        } catch (Exception ex) {
+            return 0;
+        }
+    }
+
+    private boolean mapRequired(String name, String param) {
+        String value = CacheUtil.get(String.format("%s.%s.required", name.toLowerCase(), param.toLowerCase()));
+        if (value == null)
+            return false;
+        else
+            return value.equalsIgnoreCase("true");
+    }
+
+    private String mapType(String name) {
+        String value = CacheUtil.get(String.format("%s.type", name.toLowerCase()));
+        if (value == null)
+            return "";
+        else
+            return value;
+    }
+
+    private String mapDb(String name) {
+        return CacheUtil.get(String.format("%s.db", name.toLowerCase()));
+    }
+
+    private List<String> mapParam(String name) {
+        List<String> value = CacheUtil.getModel(String.format("%s.param", name.toLowerCase()), ArrayList.class);
+        if (value == null)
+            return new ArrayList<>();
+        else
+            return value;
+    }
+
+    private boolean getResult(HttpServletResponse response, Map msg) {
+        try {
+            response.setContentType("application/Json;charset=utf-8");
+            PrintWriter pWriter = response.getWriter();
+            pWriter.write(JSON.toJSONString(msg));
+            pWriter.flush();
+            pWriter.close();
+            return false;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return true;
+        }
+    }
+}
