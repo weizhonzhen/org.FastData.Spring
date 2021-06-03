459c10f00396bc3711bcedef6d487b62ca186e42
weizhonzhen <czdswzz@126.com>
2021-06-03 14:24:30 +0800

    Add files via upload

--- src/main/java/org/FastData/Spring/Util/BaseMap.java
+++ src/main/java/org/FastData/Spring/Util/BaseMap.java
@@ -2,11 +2,12 @@ package org.FastData.Spring.Util;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public final class BaseMap {
-    public static <T> Map<String,Object> toMap(T model) {
-        Map<String, Object> result = new HashMap<String, Object>();
+    public static <T> LinkedHashMap<String,Object> toMap(T model) {
+        LinkedHashMap<String, Object> result = new LinkedHashMap<String, Object>();
         Arrays.stream(model.getClass().getDeclaredFields()).forEach(a->{
             result.put(a.getName(),ReflectUtil.get(model,a.getName(),a.getType()));
         });
