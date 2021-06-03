cf6a3b3d87aa90d6f99bf3fd0915f790b480853a
weizhonzhen <czdswzz@126.com>
2021-06-03 14:41:32 +0800

    Add files via upload

--- src/main/java/org/FastData/Spring/Util/BaseMap.java
+++ src/main/java/org/FastData/Spring/Util/BaseMap.java
@@ -1,9 +1,7 @@
 package org.FastData.Spring.Util;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.Map;
 
 public final class BaseMap {
     public static <T> LinkedHashMap<String,Object> toMap(T model) {
