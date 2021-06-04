c8dc35135ee8c3dbc8e5759d5f0db44b7a0a751a
weizhonzhen <czdswzz@126.com>
2021-06-04 13:20:40 +0800

    Add files via upload

--- src/main/java/org/FastData/Spring/Util/BaseMap.java
+++ src/main/java/org/FastData/Spring/Util/BaseMap.java
@@ -5,10 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class BaseMap {
-    public static <T> Map<String,Object> toMap(T model) {
+    public static <T> Map<String, Object> toMap(T model) {
         Map<String, Object> result = new HashMap<>();
-        Arrays.stream(model.getClass().getDeclaredFields()).forEach(a->{
-            result.put(a.getName(),ReflectUtil.get(model,a.getName(),a.getType()));
+        Arrays.stream(model.getClass().getDeclaredFields()).forEach((a) -> {
+            Object value = ReflectUtil.get(model, a.getName(), a.getType());
+            if (value != null && !value.equals(""))
+                result.put(a.getName(), value);
         });
         return result;
     }
