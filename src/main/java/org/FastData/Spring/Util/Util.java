c75c724ad941d8c678f572c4a9975e3d915079a6
weizhonzhen <czdswzz@126.com>
2021-06-15 14:48:51 +0800

    Add files via upload

--- src/main/java/org/FastData/Spring/Util/Util.java
+++ src/main/java/org/FastData/Spring/Util/Util.java
@@ -1,5 +1,8 @@
 package org.FastData.Spring.Util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class Util {
     public static boolean isNullOrEmpty(Object value) {
         return value == null || value.toString().equals("");
@@ -7,5 +10,16 @@ public final class Util {
 
     public static boolean isNullOrEmpty(String value) {
         return value == null || value.toString().equals("");
+    }
+
+    public static boolean isZhString(String str) {
+        if (isNullOrEmpty(str))
+            return false;
+        Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
+        Matcher m = p.matcher(str);
+        if (m.find())
+            return true;
+        else
+            return false;
     }
 }
