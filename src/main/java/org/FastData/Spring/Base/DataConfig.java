0e6fa9e3292a33ebb6cdc2f76b4d23175e2c16dd
weizhonzhen <czdswzz@126.com>
2021-06-22 10:26:45 +0800

    Add files via upload

--- src/main/java/org/FastData/Spring/Base/DataConfig.java
+++ src/main/java/org/FastData/Spring/Base/DataConfig.java
@@ -7,6 +7,7 @@ import org.FastData.Spring.Util.CacheUtil;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -43,7 +44,7 @@ public final class DataConfig {
         try {
             InputStream stream = Class.forName(Thread.currentThread().getStackTrace()[1].getClassName()).getResourceAsStream("/"+fileName);
             if (stream != null) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
                 while ((s = reader.readLine()) != null) {
                     sb.append(s);
                 }
