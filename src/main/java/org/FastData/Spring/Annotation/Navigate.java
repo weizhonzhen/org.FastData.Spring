132a6bcf2f56f30683a0d94b585eb6b138645994
weizhonzhen <czdswzz@126.com>
2021-09-09 17:03:48 +0800

    Add files via upload

---
+++ src/main/java/org/FastData/Spring/Annotation/Navigate.java
@@ -0,0 +1,14 @@
+package org.FastData.Spring.Annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Navigate {
+    String Appand() default "";
+
+    String Name() default "";
+}
