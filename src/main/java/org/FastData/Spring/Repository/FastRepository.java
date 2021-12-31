74ddd6be212b474bb567a17bb2247acaaf6cba82
weizhonzhen <czdswzz@126.com>
2021-12-31 09:44:10 +0800

    Add files via upload

--- src/main/java/org/FastData/Spring/Repository/FastRepository.java
+++ src/main/java/org/FastData/Spring/Repository/FastRepository.java
@@ -1261,20 +1261,3 @@ class FastProxy implements InvocationHandler {
     }
 }
 
-@Component
-class ApplicationContextRegister implements BeanFactoryPostProcessor {
-    private static ConfigurableListableBeanFactory beanFactory;
-
-    public static void setBean(Class<?> type) {
-        if (type != null && beanFactory != null) {
-            FastProxy fastProxy = new FastProxy(null);
-            beanFactory.registerSingleton(type.getName(), type.cast( fastProxy.instance(type)));
-        }
-    }
-
-    @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        ApplicationContextRegister.beanFactory = beanFactory;
-        CacheUtil.setModel("beanFactory", beanFactory);
-    }
-}
