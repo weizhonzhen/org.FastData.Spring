2f96489b3d9f165130e319fb327f3e6032390119
weizhonzhen <czdswzz@126.com>
2022-01-06 09:09:29 +0800

    Add files via upload

--- src/main/java/org/FastData/Spring/FastServiceAop/FastServiceProxy.java
+++ src/main/java/org/FastData/Spring/FastServiceAop/FastServiceProxy.java
@@ -1,13 +1,10 @@
 package org.FastData.Spring.FastServiceAop;
 
-import org.FastData.Spring.Repository.ApplicationContextRegister;
 import org.FastData.Spring.Util.CacheUtil;
-import org.FastData.Spring.Util.ScanPackage;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.List;
 
 public class FastServiceProxy {
     private ConfigurableListableBeanFactory beanFactory = CacheUtil.getModel("beanFactory", ConfigurableListableBeanFactory.class);
@@ -62,16 +59,5 @@ public class FastServiceProxy {
                 return exception.getResult();
             }
         }
-    }
-
-    public static void init(String packageName) {
-        List<Class> list = ScanPackage.get(packageName);
-        list.forEach(a -> {
-            if (!a.isInterface() && a.getInterfaces().length > 0) {
-                for (Class<?> i : a.getInterfaces()) {
-                    ApplicationContextRegister.setBean(i);
-                }
-            }
-        });
     }
 }
