2576e63323865119433ab38370645e506fb0f284
weizhonzhen <czdswzz@126.com>
2024-04-25 09:02:48 +0800

    Delete src/main/java/org/FastData/Spring/Function directory

--- src/main/java/org/FastData/Spring/Function/FastExpression.java
+++
@@ -1,8 +0,0 @@
-package org.FastData.Spring.Function;
-
-import java.io.Serializable;
-
-@FunctionalInterface
-public interface FastExpression<S, T> extends Serializable {
-    T convert(S source);
-}
